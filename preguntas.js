bbbe55a295163646eb7cca1626d67d6eb37b318d
webnecochea <56007535+webnecochea@users.noreply.github.com>
2020-08-22 03:46:24 -0300

    Update preguntas.js

--- preguntas.js
+++ preguntas.js
@@ -11,89 +11,100 @@ var listaPreguntas = [
             img : ""
         },
         {
-            consigna : "En un almacen ¿donde deben colocarse los productos de mayor rotacion?",
+            consigna : "¿Cual de los siguientes NO es motivo de aplicar logística inversa?",
             op : [
-                opcion1 = "Cerca de la entrada",
-                opcion2 = "Al fondo",
-                opcion3 = "A la derecha",
-                opcion4 = "En el centro"
+                opcion1 = "Producto defectuoso",
+                opcion2 = "Reciclaje",
+                opcion3 = "Entrega correcta",
+                opcion4 = "Envases retornables"
+            ],
+            respuesta : 3,
+            img : ""
+        },
+        {
+            consigna : "En logística ¿A que se le llama cuello de botella?",
+            op : [
+                opcion1 = "Una herramienta para manipular liquidos",
+                opcion2 = "Un gerente con problemas de alcoholismo",
+                opcion3 = "Un proceso que entorpece o limita a todo el sistema",
+                opcion4 = "Todas las anteriores"
+            ],
+            respuesta : 3,
+            img : ""
+        },
+        {
+            consigna : "¿A que hace referencia la logística verde?",
+            op : [
+                opcion1 = "Minimizar el impacto ambiental",
+                opcion2 = "Cotización del dolar",
+                opcion3 = "Transporte de vegetales",
+                opcion4 = "Técnicas que no estan maduras"
             ],
             respuesta : 1,
             img : ""
         },
         {
-            consigna : "En un almacen ¿donde deben colocarse los productos de mayor rotacion?",
+            consigna : "¿Que conocimientos debe tener un Lic. en Logistica Integral?",
             op : [
-                opcion1 = "Cerca de la entrada",
-                opcion2 = "Al fondo",
-                opcion3 = "A la derecha",
-                opcion4 = "En el centro"
+                opcion1 = "De economía",
+                opcion2 = "De Matemáticas",
+                opcion3 = "De RRHH",
+                opcion4 = "Todos los anteriores y más"
             ],
-            respuesta : 1,
+            respuesta : 4,
             img : ""
         },
         {
-            consigna : "En un almacen ¿donde deben colocarse los productos de mayor rotacion?",
+            consigna : "¿Que significan las siglas FIFO?",
             op : [
-                opcion1 = "Cerca de la entrada",
-                opcion2 = "Al fondo",
-                opcion3 = "A la derecha",
-                opcion4 = "En el centro"
+                opcion1 = "Federacion de intereses de Francia y Orlando",
+                opcion2 = "Primero en entrar, primero en salir",
+                opcion3 = "Primero en entrar, último en salir",
+                opcion4 = "Federacion internacional de fines osados"
             ],
-            respuesta : 1,
+            respuesta : 2,
             img : ""
         },
         {
-            consigna : "En un almacen ¿donde deben colocarse los productos de mayor rotacion?",
+            consigna : "¿Cual de estas NO es una de las principales funciones de un gerente?",
             op : [
-                opcion1 = "Cerca de la entrada",
-                opcion2 = "Al fondo",
-                opcion3 = "A la derecha",
-                opcion4 = "En el centro"
+                opcion1 = "Planificar",
+                opcion2 = "Organizar",
+                opcion3 = "Producir",
+                opcion4 = "Dirigir"
             ],
-            respuesta : 1,
+            respuesta : 3,
             img : ""
         },
         {
-            consigna : "En un almacen ¿donde deben colocarse los productos de mayor rotacion?",
+            consigna : "¿Donde es conveniente instalar una terminal de mercancias?",
             op : [
-                opcion1 = "Cerca de la entrada",
-                opcion2 = "Al fondo",
-                opcion3 = "A la derecha",
-                opcion4 = "En el centro"
+                opcion1 = "En el centro de la ciudad",
+                opcion2 = "En las periferias",
+                opcion3 = "Lejos de las rutas",
+                opcion4 = "En lugares altos, como cerros o montañas"
             ],
-            respuesta : 1,
+            respuesta : 2,
             img : ""
         },
         {
-            consigna : "En un almacen ¿donde deben colocarse los productos de mayor rotacion?",
+            consigna : "¿En que ambito nacio la logística?",
             op : [
-                opcion1 = "Cerca de la entrada",
-                opcion2 = "Al fondo",
-                opcion3 = "A la derecha",
-                opcion4 = "En el centro"
+                opcion1 = "En los deportes",
+                opcion2 = "En los negocios",
+                opcion3 = "En la internet",
+                opcion4 = "En la guerra"
             ],
-            respuesta : 1,
+            respuesta : 4,
             img : ""
         },
         {
-            consigna : "En un almacen ¿donde deben colocarse los productos de mayor rotacion?",
+            consigna : "¿Cuales de estos factores favorecieron la globalizacion del mercado?",
             op : [
-                opcion1 = "Cerca de la entrada",
-                opcion2 = "Al fondo",
-                opcion3 = "A la derecha",
-                opcion4 = "En el centro"
-            ],
-            respuesta : 1,
-            img : ""
-        },
-        {
-            consigna : "TE AMO LULITA PAPITAS",
-            op : [
-                opcion1 = "POR SIEMPRE",
-                opcion2 = "PARA TODAS LAS VIDAS",
-                opcion3 = "HASTA EL INFINITO",
-                opcion4 = "TODAS LAS ANTERIORES"
+                opcion1 = "Internet",
+                opcion2 = "Los contenedores",
+                opcion3 = "La construccion de corredores y canales",
+                opcion4 = "Todas las anteriores"
             ],
             respuesta : 4,
             img : ""
